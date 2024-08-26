9e61bd93164d32c690f8463f4ae57ffc007ab5b7
batumzff <batuhanmuzafferoglu@gmail.com>
2024-08-26 21:10:42 +0300

    error page updated

--- src/Components/ERROR-PAGE/ErrorPage.jsx
+++ src/Components/ERROR-PAGE/ErrorPage.jsx
@@ -2,23 +2,24 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { clearError } from '../../Features/authSlice';
+import { clearBlogError } from '../../Features/BlogSlice';
 import style from "./ErrorStyle.module.scss";
 
-const ErrorPage = () => {
+const ErrorPage = ({ msg, blogError }) => {
     const { errorMessage, error } = useSelector((state) => state.auth);
     const [message, setMessage] = useState("");
 
     const dispatch = useDispatch();
     const location = useLocation()
 
-    console.log(location.pathname);
+    // console.log(location.pathname);
 
     const path = location?.pathname.split("/")[1]
 
     useEffect(() => {
         let timer;
     
-        switch (error) {
+        switch (error || blogError) {
           case errorMessage?.includes("duplicate") &&
             errorMessage?.includes("username"):
             setMessage("Username has taken");
@@ -45,16 +46,74 @@ const ErrorPage = () => {
             }, 3000);
     
             return () => clearTimeout(timer);
-    
-          default:
-            setMessage("Sorry there is an error occurred just wait for 3 seconds");
+
+          case errorMessage?.includes("check your username/email and password"):
+            setMessage(" Credentials are wrong please check your username/email and password");
             timer = setTimeout(() => {
               dispatch(clearError());
             }, 3000);
     
             return () => clearTimeout(timer);
+
+            case msg?.includes("There is no such a blog"):
+              setMessage("Sorry the Blog you are looking for is deleted by the owner");
+              timer = setTimeout(() => {
+                dispatch(clearBlogError());
+              }, 3000);
+      
+              return () => clearTimeout(timer);
+             
+      
+            case msg?.includes("The blog you are looking for has been removed or deleted"):
+              setMessage("The blog you are looking for has been deleted");
+              timer = setTimeout(() => {
+                dispatch(clearBlogError());
+              }, 3000);
+      
+              return () => clearTimeout(timer);
+  
+            case msg?.includes("E11000 duplicate key error collection: blogAPI.categories"):
+              setMessage("The category already exists");
+              timer = setTimeout(() => {
+                dispatch(clearBlogError());
+              }, 3000);
+      
+              return () => clearTimeout(timer);
+  
+            case msg?.includes("Comment not found"):
+              setMessage("Sorry there is no such a comment");
+              timer = setTimeout(() => {
+                dispatch(clearBlogError());
+              }, 3000);
+      
+              return () => clearTimeout(timer);
+  
+            case msg?.includes("Blog not found"):
+              setMessage("Blog not found");
+              timer = setTimeout(() => {
+                dispatch(clearBlogError());
+              }, 3000);
+      
+              return () => clearTimeout(timer);
+      
+      
+          default:
+            setMessage("Sorry there is an error occurred just wait for 3 seconds");
+            timer = setTimeout(() => {
+              dispatch(clearError());
+              dispatch(clearBlogError());
+            }, 3000);
+    
+            return () => clearTimeout(timer);
         }
-      }, [error]);
+      }, [error, blogError]);
+
+      
+      
+      // console.log("error from auth : ",error);
+      // console.log("errorMessage from auth : ",errorMessage);
+      // console.log("msg from blogError : ",msg);
+      // console.log("blogError from blogError : ",blogError);
     
   return (
     <section className={style.main}>
--- src/Pages/CATEGORIES/Categories.jsx
+++ src/Pages/CATEGORIES/Categories.jsx
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import useBlogData from "../../Custom-hooks/useBlogData";
+import ErrorPage from "../../Components/ERROR-PAGE/ErrorPage";
 import { useNavigate } from "react-router-dom";
-import categoriesStyle from "./Categories.module.scss";
-import useAxios from "../../Custom-hooks/useAxios";
 import { VscEdit } from "react-icons/vsc";
+import useAxios from "../../Custom-hooks/useAxios";
+import useDebounce from "../../Custom-hooks/useDebounce";
+import { fetchFail } from "../../Features/BlogSlice";
+import style from "./Categories.module.scss";
 
 const Categories = () => {
-  const { categories } = useSelector((state) => state.blog);
-  const { user } = useSelector((state) => state.auth);
+  const { user, token } = useSelector((state) => state.auth);
+  const { categories, blogErrorMessage, error} = useSelector(
+    (state) => state.blog
+  );
+  const dispatch = useDispatch()
   const { getData, getCategoryById } = useBlogData();
   const { axiosWithToken } = useAxios();
-  const navigate = useNavigate();
-  const [add, setAdd] = useState("");
+  const [categoryId, setCategoryId] = useState("");
+  const [categoryName, setCategoryName] = useState("");
   const [edit, setEdit] = useState(false);
-  const [editId, setEditId] = useState("");
+  const navigate = useNavigate();
+
+  const debouncedName = useDebounce(categoryName, 500);
 
   useEffect(() => {
     getData("categories");
@@ -24,84 +32,98 @@ const Categories = () => {
 
   const handleClick = async (categoryId) => {
     // console.log(categoryId);
+    setCategoryId(categoryId);
     try {
       const data = await getCategoryById("categoryDetail", categoryId);
       // console.log(data);
-      navigate("/category-detail");
+      navigate(`/category-detail/${categoryId}`);
     } catch (error) {
       console.log(error);
     }
   };
-  const addCategory = async (e) => {
-    e.preventDefault();
-    const postData = { name: add };
-    const data = await axiosWithToken.post("categories", postData);
-    // console.log(data)
-    getData("categories");
-  };
-  // console.log(add)
 
-  const handleEditChange = (id) => {
+  const handleChange = (e) => {
+    setCategoryName(e.target.value);
+  };
+
+  const addCategory = async () => {
+    console.log(categoryName);
+
+    const postData = { name: debouncedName };
+    try {
+      const data = await axiosWithToken.post("categories", postData);
+    console.log(data);
+    getData("categories");
+    setCategoryName("");
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail(error))
+    }
+    
+  };
+
+  const getEditInfo = async (id, name) => {
     setEdit(true);
-    const info = categories.filter((category) => category._id == id);
-    // console.log(info);
-    // console.log(info[0]._id)
-    let name = info[0].name;
-    // console.log(name);
-    setAdd(name);
-    setEditId(info[0]._id);
+    setCategoryName(name);
+    setCategoryId(id);
+    console.log(id);
+    console.log(name);
   };
-  // console.log(add)
-  const handleCategoryEdit = async () => {
-    const postData = { categoryId: editId, name: add };
-    // console.log(postData);
 
-    const data = await axiosWithToken.put(`categories/${editId}`, postData);
-    // console.log(data);
+  const handleEdit = async () => {
+    const postData = { categoryId, name: categoryName };
+    console.log(postData);
+    const { data } = await axiosWithToken.put(
+      `categories/${categoryId}`,
+      postData
+    );
+    console.log(data);
     getData("categories");
-    setAdd("");
     setEdit(false);
+    setCategoryName("");
   };
+
+  // console.log(categoryId);
+  // console.log(categoryName);
 
   return (
-    <div className={categoriesStyle.container}>
-      <div>
+    <section className={style.container}>
+      <main>
         <h3>Your site Your Choice</h3>
-        <div className={categoriesStyle.categories}>
-          {categories?.map((category) => (
-            <div key={category._id}>
-              <h3 onClick={() => handleClick(category._id)}>
-                {category.name}{" "}
-              </h3>
-              {(user?.isAdmin || user?.isStaff) && (
-                <span>
-                  <VscEdit
-                    onClick={() => handleEditChange(category?._id)}
-                    color="green"
-                  />
-                </span>
-              )}{" "}
-            </div>
-          ))}
-        </div>
-        <div>
-          <input
-            type="text"
-            name="category"
-            value={add}
-            onChange={(e) => setAdd(e.target.value)}
-          />
-          {
-            <button
-              onClick={edit ? handleEdit : addCategory}
-              disabled={add.trim() == "" && "disabled"}
-            >
-              {edit ? "Edit Category" : "Add Category"}
-            </button>
-          }
-        </div>
-      </div>
-    </div>
+        <section className={style.categories}>
+          {error ? (
+            <ErrorPage msg={blogErrorMessage} blogError={error} />
+          ) : (
+            categories?.map((category) => (
+              <>
+                <h3 onClick={() => handleClick(category._id)}>
+                  {category.name}{" "}
+                </h3>
+                {user && (user.isAdmin || user.isStaff) && (
+                  <div onClick={() => getEditInfo(category._id, category.name)}>
+                    <VscEdit />
+                  </div>
+                )}
+              </>
+            ))
+          )}
+        </section>
+        <section className={style.add}>
+          <h4>{edit ? "Edit Category" : "Add Category"}</h4>
+          {token && (
+            <>
+              <input type="text" value={categoryName} onChange={handleChange} />
+              <button
+                onClick={edit ? handleEdit : addCategory}
+                disabled={categoryName.trim() == "" && "disabled"}
+              >
+                {edit ? "Edit Category" : "Add Category"}
+              </button>
+            </>
+          )}
+        </section>
+      </main>
+    </section>
   );
 };
 
